ecdc6e97e54342a49d9ef5cb633df6e55304caa8
LENOVO USER <talyagross@gmail.com>
2022-10-19 14:58:30 +0300

    new version

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import hashlib
 
-result = hashlib.md5(b'9')
+result = hashlib.md5(b'9999')
 # printing the equivalent byte value.
 print("The byte equivalent of hash is : ", end="")
 print(result.digest())
 result = result.digest()
 
 digit = 0
-for i in range(1000000000, 10000000000):
+for i in range(10000):
     digit = f'{i:03}'
     num = hashlib.md5(digit.encode())
     print(digit)
